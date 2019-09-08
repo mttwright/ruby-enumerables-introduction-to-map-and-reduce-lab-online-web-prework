0b419bf617716ff5db4e222d647c0fd7cd21d42c
virtual-frame-7789 <mttwright@gmail.com>
2019-09-08 16:31:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,5 +27,18 @@ def map_to_no_change(source_array)
   return_array
 end
 
+def map_to_negativize(source_array)
+  
+  return_array = []
+  counter = 0
+  
+  while source_array[counter] do
+    return_array[counter] = source_array[counter] * 2
+    counter += 1
+  end
+  
+  return_array
+end
+
 
 
